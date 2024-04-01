6c6aecce40542ee858bd78063ddaf77f171d919e
Sheeyx <sheeyx00@gmail.com>
2024-04-02 04:37:26 +0900

    fix: organize homepage related sectional component

---
+++ src/screens/homePage/Advertisement.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function Advertiement() {
+  return (
+    <div>
+        <Container>
+            Advertiement
+        </Container>
+    </div>
+  );
+}
---
+++ src/screens/homePage/Events.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function Events() {
+  return (
+    <div>
+        <Container>
+        Events
+        </Container>
+    </div>
+  );
+}
